63fade260383c0c9c4c0a3c67c8dc81c5927cb08
lpowis97 <lpowis@tervis.com>
2016-09-08 11:28:48 -0400

    Renamed and added new functions
    Renamed Add-IPAddressToTrustedHosts to Add-IPAddressToWSManTrustedHosts
    Added Get-WSManTrustedHosts
    Added Enter-PSSessionsToNewEndpoint

--- TervisNewEndpoint.psm1
+++ TervisNewEndpoint.psm1
@@ -1,8 +1,24 @@
-﻿function Add-IPAddressToTrustedHosts {
-    param (
-    [string][parameter(Mandatory, ValueFromPipeline)]$IPAddress
+﻿function Add-IPAddressToWSManTrustedHosts {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory, ValueFromPipeline)]$IPAddress
     )
 
     Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value $IPAddress -Force
 }
 
+function Get-WSManTrustedHosts {
+
+    Get-Item -Path WSMan:\localhost\Client\TrustedHosts
+
+}
+
+function Enter-PSSessionToNewEndpoint {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory, ValueFromPipeline)]$IPAddress
+    )    
+    $Credentials = Get-Credential
+
+    Enter-PSSession -ComputerName $IPAddress -Credential $Credentials
+}
