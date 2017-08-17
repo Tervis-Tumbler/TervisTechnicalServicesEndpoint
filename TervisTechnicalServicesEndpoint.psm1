fb6b34a9334d0770dd3b2e40658b7964573c6244
hambone124 <hpe60m5@gmail.com>
2017-08-17 17:05:49 -0400

    Manual PowerCfg is Surface detected

--- TervisTechnicalServicesEndpoint.psm1
+++ TervisTechnicalServicesEndpoint.psm1
@@ -365,12 +365,15 @@ function Set-TervisEndpointPowerPlan {
         powercfg -S $PowerPlanGUID
         $ActivePowerScheme = powercfg /getactivescheme
         $ActivePowerScheme
-        powercfg -change -monitor-timeout-ac 0
-        powercfg -change -standby-timeout-ac 0
-        powercfg -change -hibernate-timeout-ac 0  
-        powercfg -change -monitor-timeout-dc 0
-        powercfg -change -standby-timeout-dc 0
-        powercfg -change -hibernate-timeout-dc 0  
+        
+        if ((Get-ComputerInfo).CsModel -match "Surface") {
+            powercfg -change -monitor-timeout-ac 0
+            powercfg -change -standby-timeout-ac 0
+            powercfg -change -hibernate-timeout-ac 0  
+            powercfg -change -monitor-timeout-dc 0
+            powercfg -change -standby-timeout-dc 0
+            powercfg -change -hibernate-timeout-dc 0
+        }
     }
 
     Write-Verbose $ActivePowerScheme
