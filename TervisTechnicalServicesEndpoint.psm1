29b9f7f1be1307494675334d68c08ea921c1f5d6
hambone124 <hpe60m5@gmail.com>
2017-08-17 16:55:54 -0400

    PowerCfg fallback for Surface Pro

--- TervisTechnicalServicesEndpoint.psm1
+++ TervisTechnicalServicesEndpoint.psm1
@@ -364,7 +364,13 @@ function Set-TervisEndpointPowerPlan {
         $PowerPlanGUID = $PowerPlanInstanceID.split("{")[1].split("}")[0]
         powercfg -S $PowerPlanGUID
         $ActivePowerScheme = powercfg /getactivescheme
-        $ActivePowerScheme  
+        $ActivePowerScheme
+        powercfg -change -monitor-timeout-ac 0
+        powercfg -change -standby-timeout-ac 0
+        powercfg -change -hibernate-timeout-ac 0  
+        powercfg -change -monitor-timeout-dc 0
+        powercfg -change -standby-timeout-dc 0
+        powercfg -change -hibernate-timeout-dc 0  
     }
 
     Write-Verbose $ActivePowerScheme
